46f3bf9e22a24d9127451f916f96e5eebaca6566
Zachary Springer <zachar7@pdx.edu>
2025-05-06 19:54:26 -0700

    additional styling changes

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -76,7 +76,7 @@ export default async function Page(props: {
           </div>
         </div>
         
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
